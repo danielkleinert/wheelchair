e0a43f68c712c6ddccf7da4c06e3cb1b9aae1226
Daniel Kleinert <daniel@pristinebytes.com>
2022-05-11 21:01:17 +0200

    simplify arduino code a bunch

--- arduino/src/main.cpp
+++ arduino/src/main.cpp
@@ -1,48 +1,30 @@
 #include <Arduino.h>
-#include <EnableInterrupt.h>
+#include <Encoder.h>
+#include <arduino-timer.h>
+
+auto timer = timer_create_default();
 
 const volatile long SAMPLING_RATE = 10;
-const volatile long DEBOUNCE_MILLIS = 2;
-const volatile int LEFT = 0;
-const volatile int RIGHT = 1;
-const volatile int CLK_PIN[2] = {3, 5};
-const volatile int DATA_PIN[2] = {2, 4};
+//   Best Performance: both pins have interrupt capability (2, 3)
+//   Good Performance: only the first pin has interrupt capability
+//   Low Performance:  neither pin has interrupt capability
+Encoder leftWheel(3, 5);
+Encoder rightWheel(2, 4);
 
-volatile int ticks[2] = {0, 0};
-volatile unsigned long lastChanged[2] = {0, 0};
-
-void stepped(int wheelIndex) {
-    const unsigned long currentMillis = millis();
-    if (lastChanged[wheelIndex] + DEBOUNCE_MILLIS > currentMillis) return;
-    const int direction_rectifier = wheelIndex ? 1 : -1; // forward is different as axles point in opposite directions
-    ticks[wheelIndex] += (digitalRead(DATA_PIN[wheelIndex]) ? 1 : -1) * direction_rectifier;
-    lastChanged[wheelIndex] = currentMillis;
-}
-
-void steppedLeft() {
-    stepped(LEFT);
-}
-
-void steppedRight() {
-    stepped(RIGHT);
+bool sendData(void *) {
+    Serial.print(leftWheel.read());
+    Serial.print(",");
+    Serial.println(-rightWheel.read());
+    rightWheel.write(0);
+    leftWheel.write(0);
+    return true; // keep timer active
 }
 
 void setup() {
-    pinMode(DATA_PIN[LEFT], INPUT_PULLUP);
-    pinMode(CLK_PIN[LEFT], INPUT_PULLUP);
-    enableInterrupt(CLK_PIN[LEFT], steppedLeft, FALLING);    pinMode(DATA_PIN[LEFT], INPUT_PULLUP);
-    pinMode(DATA_PIN[RIGHT], INPUT_PULLUP);
-    pinMode(CLK_PIN[RIGHT], INPUT_PULLUP);
-    enableInterrupt(CLK_PIN[RIGHT], steppedRight, FALLING);
-
     Serial.begin(57600);
+    timer.every(1000/SAMPLING_RATE, sendData);
 }
 
 void loop() {
-    Serial.print(ticks[LEFT]);
-    Serial.print(",");
-    Serial.println(ticks[RIGHT]);
-    ticks[LEFT] = ticks[RIGHT] = 0;
-    delay(1000/SAMPLING_RATE);
+    timer.tick();
 }
-
