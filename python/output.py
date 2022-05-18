6a2a3391f15950911fa1e421f4a55a74fe6d6e1d
Daniel Kleinert <daniel@pristinebytes.com>
2022-05-18 11:51:30 +0200

    more fixes

--- python/output.py
+++ python/output.py
@@ -6,7 +6,7 @@ try:
     from blessings import Terminal
     term = Terminal()
 except:
-    pass
+    term = None
 
 SAMPLING_RATE = 30
 DEADZONE = 10
@@ -17,14 +17,14 @@ FORWARD_PRESS_TIME = (1 / SAMPLING_RATE) * 0.9
 class KeyboardOutput:
 
     def __init__(self) -> None:
-        if Terminal:
+        if term:
             print(term.clear)
         self.keyboard = Controller()
         self.release_times = {}
         asyncio.create_task(self.check_key_up())
 
     def send_control(self, steering: Steering):
-        if Terminal:
+        if term:
             print(term.normal, term.move(0, 0), term.clear_eol, steering.forward, term.move(0, 8), steering.right)
         if steering.forward > DEADZONE:
             self.press('u', steering.forward * FORWARD_PRESS_TIME)
@@ -43,13 +43,13 @@ class KeyboardOutput:
 
     async def check_key_up(self):
         while True:
-            if Terminal:
+            if term:
                 print(term.move(0, 1))
             for key in self.release_times.keys():
                 release_time = self.release_times[key]
                 if release_time is not None and release_time < datetime.now():
                     self.keyboard.release(key)
                     self.release_times[key] = None
-                if Terminal:
+                if term:
                     print((term.on_blue if release_time else "") + f" {key} " + term.normal)
             await asyncio.sleep(1 / 60)
